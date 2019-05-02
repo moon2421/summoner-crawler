67fe1d4990b358a3b7b83b1d29239b912e612511
moon2421 <47843410+moon2421@users.noreply.github.com>
2019-05-02 21:51:55 +0900

    Main file
    add Main file

---
+++ Summoner-crawler/main/java/org/cnu/realcoding/summoner/Controller/SummonerController.java
@@ -0,0 +1,9 @@
+package org.cnu.realcoding.summoner.Controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+@RestController
+public class SummonerController {
+    @Autowired
+    
+}

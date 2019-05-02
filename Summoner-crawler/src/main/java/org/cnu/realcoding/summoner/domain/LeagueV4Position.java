855ccf6c9bc590c1a92a7664ce37d4c6a45263f2
minkinew <dbs03088@naver.com>
2019-05-02 23:12:31 +0900

    Domain information Code add

---
+++ Summoner-crawler/src/main/java/org/cnu/realcoding/summoner/domain/LeagueV4Position.java
@@ -0,0 +1,20 @@
+package org.cnu.realcoding.summoner.domain;
+
+public class LeagueV4Position {
+    String leagueId;
+    String leagueName;
+    String queueType;
+    String position;
+    String tier;
+    String rank;
+    String summonerID;
+    String summonerName;
+    int leaguePoints;
+    int wins;
+    int losses;
+    boolean veteran;
+    boolean inactive;
+    boolean freshBlood;
+    boolean hotStreak;
+}
+
---
+++ Summoner-crawler/src/main/java/org/cnu/realcoding/summoner/repository/SummonerV4.java
@@ -0,0 +1,12 @@
+package org.cnu.realcoding.summoner.repository;
+
+public class SummonerV4 {
+    String id;
+    String accountId;
+    String puuid;
+    String name;
+    int profileIconId;
+    long revisionDate;
+    int summonerLevel;
+}
+
